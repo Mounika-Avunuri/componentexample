090b0210aaeb2fa4195e816331ac42f0980d6dbc
mounuikavedhanshi <89015579+mounuikavedhanshi@users.noreply.github.com>
2022-02-18 13:35:07 +0530

    adding files

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -1,15 +1,33 @@
 import Input from "../Input/Input";
 import Output from "../Output/Output";
-import {useState} from 'react'
+import {useState} from 'react';
+//import './InputText.css';
+import styles from './InputText.module.css'
 
 const InputText = () =>{
     const[textVal,setTextVal] = useState('ABC');
-const inputChangeHandler
+const inputChangeHandler = (event)=>{const textvalue=event.target.value
+    setTextVal(textvalue)
+}
+
+const msgStyleObj = {
+    color:'yellow',
+    fontWeight:'bold',
+    paddingTop:'20px'
+}
+
+const name = 'abcd';
+const message = (<div style={{color:'green',fontWeight:'bold'}}>Hello {name}</div>);
+const myname = 'Mounika';
+const fullname = (<div style={msgStyleObj}> Aileni {myname}</div>)
     return(
         <>
-    <Input value={textVal} onInputChange={(event)=>{const textvalue=event.target.value
-    setTextVal(textvalue)}}/>
+        <div className={styles.container}>
+        {message}
+        {fullname}
+    <Input value={textVal} onInputChange={inputChangeHandler}/>
     <Output value={textVal}/>
+    </div>
     </>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
+//import './Home.css'
 import InputText from '../components/InputText/InputText';
+import styles from './Home.module.css';
 const Home = () =>{
     return(
 
-        <div>
+        <div className={styles.container}>
         <h4>Home Page</h4>
         <InputText/>
         </div>
